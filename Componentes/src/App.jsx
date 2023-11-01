9a6b79845645c38d0075b998ca9831a65e902279
Ivan Romero Gutierrez <manny.parte.2@gmail.com>
2023-10-31 23:17:33 -0400

    1 completo

--- Componentes/src/App.jsx
+++ Componentes/src/App.jsx
@@ -2,12 +2,16 @@
 import './App.css'
 import Fisrtcomponent from './assets/components/fisrtcomponent'
 import Secomponent from './assets/components/Secomponent'
+import MiComponente from './assets/components/ClassComp'
+
+
 function App() {
 
   return (
     <>
       <Fisrtcomponent/>
       <Secomponent/>
+      <MiComponente/>
     </>
   )
 }
---
+++ Componentes/src/assets/components/ClassComp.jsx
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+export default class MiComponente extends Component {
+    render(){
+        return(
+            <div>Este es un componente basado en clase</div>
+        )
+    }
+}
